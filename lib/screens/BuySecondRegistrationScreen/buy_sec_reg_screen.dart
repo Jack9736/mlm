d617ede5e875a456116653d20c9e6782d974557d
Jack9736 <88305220+Jack9736@users.noreply.github.com>
2022-08-24 17:11:00 +1000

    UPDATE=> Radio Button in sign-up-step-one. Scrolling Changes

--- lib/screens/BuySecondRegistrationScreen/buy_sec_reg_screen.dart
+++ lib/screens/BuySecondRegistrationScreen/buy_sec_reg_screen.dart
@@ -52,69 +52,272 @@ class _BuySecRegScreenState extends State<BuySecRegScreen> {
         ),
         body: SafeArea(
           bottom: true,
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Container(
-                color: Colors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                        width: double.infinity,
-                        height: 70,
-                        color:AppConstant.topHeaderBlueClr,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          // crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            IconButton(
-                              icon: const Icon(Icons.arrow_back, color: Colors.white),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
+          child: Form(
+            key: _formKey,
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                      width: double.infinity,
+                      height: 70,
+                      color:AppConstant.topHeaderBlueClr,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.arrow_back, color: Colors.white),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
 
-                            Center(
-                              child: Align(
-                                alignment: Alignment.center,
-                                child: Text(
-                                  'Create Account',
-                                  style: TextStyle(
-                                      fontSize: 20.0,
-                                      fontWeight: FontWeight.w600,
-                                      fontFamily: 'Gibson',
-                                      color: Colors.white),
-                                ),
+                          Center(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                'Create Account',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'Gibson',
+                                    color: Colors.white),
                               ),
                             ),
-                            SizedBox(height: 25, width: 25,),
+                          ),
+                          SizedBox(height: 25, width: 25,),
 
-                          ],
-                        ),
+                        ],
+                      ),
 
-                    ),
+                  ),
 
-                    signUpText(),
-                    const SizedBox(height: 15),
-                    Container(
-                      margin: EdgeInsets.all(30),
-                      child: Text('Have you got kids?') ,
-                    ),
-                    Radio(
-                      value: 1,
-                      groupValue: radioGroup,
-                      onChanged: (T){
-                        print(T);
 
-                        setState(() {
-                          
-                        });
-                      },
-                    )
-                  ],
-                ),
+                  Expanded(
+                      child: Container(
+                          color: Colors.white,
+                          padding: const EdgeInsets.only(
+                            left: 15.0, right: 15.0, top: 20.0,),
+
+                          child:Container(
+                            color: Colors.white,
+                            child: ListView(
+                              shrinkWrap: true,
+                                children:[
+                                  signUpText(),
+                                  const SizedBox(height: 15),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 20, bottom: 0.0, left: 30.0, right: 0.0),
+                                    child: Text('Have you got kids?') ,
+                                  ),
+                                  Column
+                                    (
+                                      children:[
+                                        RadioListTile(
+                                          dense:true,
+                                          value: 1,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('Kids under 5 years',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+                                            setState(() {
+                                            });
+
+                                          },
+                                        ),
+                                        RadioListTile(
+                                          dense:true,
+                                          value: 2,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('Kids 5-12 years',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+                                            setState(() {
+                                            });
+                                          },
+                                        ),
+                                        RadioListTile(
+                                          value: 3,
+                                          dense:true,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('Teenagers',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+
+                                          },
+                                        )
+                                      ]
+
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 10, bottom: 0.0, left: 30.0, right: 0.0),
+                                    child: Text('What type of dwelling do you live in?') ,
+                                  ),
+                                  Column
+                                    (
+                                      children:[
+                                        RadioListTile(
+                                          dense:true,
+                                          value: 1,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('House',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+                                            setState(() {
+                                            });
+
+                                          },
+                                        ),
+                                        RadioListTile(
+                                          value: 2,
+                                          dense:true,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('Apartment',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+                                            setState(() {
+                                            });
+                                          },
+                                        ),
+                                        RadioListTile(
+                                          value: 3,
+                                          dense:true,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('Acreage',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+
+                                          },
+                                        )
+                                      ]
+
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 10, bottom: 0.0, left: 30.0, right: 0.0),
+                                    child: Text('Any other pets') ,
+                                  ),
+                                  Column
+                                    (
+                                      children:[
+                                        RadioListTile(
+                                          value: 1,
+                                          dense:true,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('Yes',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+                                            setState(() {
+                                            });
+
+                                          },
+                                        ),
+                                        RadioListTile(
+                                          value: 2,
+                                          dense:true,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('No',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+                                            setState(() {
+                                            });
+                                          },
+                                        ),
+
+                                      ]
+
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.only(top: 10, bottom: 0.0, left: 30.0, right: 0.0),
+                                    child: Text('Will you insure your pet?') ,
+                                  ),
+                                  Column
+                                    (
+                                      children:[
+                                        RadioListTile(
+                                          value: 1,
+                                          dense:true,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('Yes',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+                                            setState(() {
+                                            });
+
+                                          },
+                                        ),
+                                        RadioListTile(
+                                          value: 2,
+                                          dense:true,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('No',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+                                            setState(() {
+                                            });
+                                          },
+                                        ),
+                                        RadioListTile(
+                                         // contentPadding: EdgeInsets.only(top: -10.0, bottom: 0.0,),
+
+                                          value: 2,
+                                          dense:true,
+                                          groupValue: radioGroup,
+                                          title: Transform.translate(
+                                              offset: const Offset(-20, 0),
+                                              child: Text('Not sure',
+                                                  style: TextStyle(fontSize: 14))),
+                                          onChanged: (T){
+                                            print(T);
+                                            setState(() {
+                                            });
+                                          },
+                                        ),
+                                      ]
+
+                                  ),
+
+                                  nextButton(context),
+                              ]
+
+                            ),
+                          )
+
+                  ))
+
+                ],
               ),
             ),
           ),
@@ -135,7 +338,7 @@ class _BuySecRegScreenState extends State<BuySecRegScreen> {
   /* Container Sign Up Text */
   Container signUpText() {
     return Container(
-        margin: EdgeInsets.only(top:30.0, bottom: 0.0, left: 30.0, right: 0.0),
+        margin: EdgeInsets.only(top:0.0, bottom: 0.0, left: 30.0, right: 0.0),
         width: double.infinity,
         child: Align(
           alignment: Alignment.topLeft,
@@ -148,7 +351,53 @@ class _BuySecRegScreenState extends State<BuySecRegScreen> {
   }
 
 
-
+  /* Next Button */
+  Container nextButton(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 0, left: 30, right: 30),
+      height: 60,
+      width: double.infinity,
+      child: Obx(() => TextButton(
+        child: controller.isApiRunning.value
+            ? Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            SizedBox(
+              height: 30,
+              width: 30,
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.white,
+                strokeWidth: 2,
+              ),
+            ),
+          ],
+        )
+            : const Text('NEXT', ),
+        style: TextButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(13.0),
+                side: BorderSide(color: AppConstant.submitBtnClr)
+            ),
+            primary: Colors.white,
+            backgroundColor: AppConstant.submitBtnClr,
+            textStyle:
+            const TextStyle(fontSize: 18, fontFamily: 'Montserrat', fontWeight: FontWeight.bold)
+        ),
+        onPressed: () {
+          Get.toNamed(AppConstant.ROUTE_BUY_SEC_REG);
+          if (_formKey.currentState!.validate()) {
+            _formKey.currentState!.save();
+            setState(() {
+              // isApiRunning = true;
+            });
+            setState(() {
+              // getTokens();
+            });
+          }
+        },
+      )),
+    );
+  }
 
 
 }
